e84212ed42b05eede63b0322543493b30831931b
RonitHamer <47880577+RonitHamer@users.noreply.github.com>
2019-03-10 11:50:41 +0200

    Add files via upload

---
+++ SelenuimCourse/Lesson01/MainClassTest.java
@@ -0,0 +1,31 @@
+package Lesson01;
+
+//import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class MainClassTest {
+  @BeforeMethod //annotations
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+
+  @Test
+  public void main() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
